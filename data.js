3a1390d1ec7d34aff87d0bc599d2c1ae733a6c85
tawhid0106 <islamtawhid431@gmail.com>
2026-07-31 01:07:49 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const database = {
+
+    "tawhid":{
+        title:"Tawhid Islam",
+        text:"The most special person ❤️"
+    },
+
+    "love":{
+        title:"Love",
+        text:"আমি তোমাকে অনেক ভালোবাসি। ❤️"
+    },
+
+    "first meet":{
+        title:"Our First Meet",
+        text:"আজকের দিনটা আমাদের জীবনের সবচেয়ে সুন্দর স্মৃতি।"
+    }
+
+};
